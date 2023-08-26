c0f5848469a0552982542e5999151139d43c15bb
AJ <94884406+0rcino@users.noreply.github.com>
2023-08-26 20:06:59 +0800

    new fix code in geometric sequence

---
+++ Sequence.cpp
@@ -0,0 +1,20 @@
+// Geometric Sequence
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, a, r, i;
+  cout << "Enter the number: ";
+  cin >> n;
+  cout << "Enter the First term :";
+  cin >> a;
+  cout << "Enter the common ratio :";
+  cin >> r;
+  for (i = 1; i <= n; i++)
+  {
+    cout << " " << a << "";
+    a = a * r;
+  }
+  return 0;
+}
